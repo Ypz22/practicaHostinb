92578a5187d351bfbdbbf8c7de8c30c9c3076fa5
Jefferson Yepez <jeffersonyepez621@gmail.com>
2026-02-10 07:23:34 -0500

    practica

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            parserOptions: {
+                sourceType: "module",
+                ecmaVersion: "latest",
+            },
+        },
+        rules: {
+            "semi": ["error", "always"],
+            "quotes": ["error", "single"],
+        },
+    }
+]
